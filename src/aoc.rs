728d132c1da049fc8e82c0a2d7c88276b766df76
Fredrik Lindroth <fredriklindroth@gmail.com>
2023-11-29 22:58:15 +0100

    Start work on cache

--- src/aoc.rs
+++ src/aoc.rs
@@ -1,8 +1,21 @@
-use std::io::{BufRead, BufReader};
+use std::{io::{BufRead, BufReader}, path::Path};
 
 use reqwest::StatusCode;
 
 pub fn get_input(year: i32, day: i32) -> Result<Vec<String>, AocError> {
+    create_cache_dir()?;
+    let file_path = format!(".cache/{year}-{day}.txt");
+    if Path::exists(Path::new(".cache/{}-{}.txt")) {
+        let mut result = Vec::<String>::new();
+        for line in std::fs::read_to_string(file_path)?.lines() {
+            result.push(line.to_string());
+        }
+        return Ok(result);
+    }
+    return get_input_from_http_and_cache(year, day);
+}
+
+pub fn get_input_from_http_and_cache(year: i32, day: i32) -> Result<Vec<String>, AocError> {
     let session_cookie = read_session_cookie()?;
     let url = format!("https://adventofcode.com/{year}/day/{day}/input");
 
@@ -20,11 +33,19 @@ pub fn get_input(year: i32, day: i32) -> Result<Vec<String>, AocError> {
     for line in reader.lines() {
         res.push(line?);
     }
+    // TODO: Save cache stuff
     Ok(res)
 }
 
 fn read_session_cookie() -> Result<String, AocError> {
     Ok(std::fs::read_to_string("session.txt")?)
+}
+
+fn create_cache_dir() -> std::io::Result<()> {
+    if Path::new(".cache").is_dir() {
+        return Ok(());
+    }
+    Ok(std::fs::create_dir(".cache")?)
 }
 
 #[derive(Debug)]
--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,5 @@
 use aoc_2023::aoc;
 fn main() {
-    let input = aoc::get_input(2023,1).expect("Could not get input");
+    let input = aoc::get_input(2022,1).expect("Could not get input");
     println!("Input: {:?}", input);
 }
